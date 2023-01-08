eccb2b524687f6745656532886c5abb24ae1b78b
Voroshin Dmitriy <voroshin@hostco.ru>
2023-01-08 21:18:40 +0300

    1 задача решена

---
+++ home_work_1/task_1/solution.py
@@ -0,0 +1,18 @@
+number_day = int(input('Введите номер дня недели: '))
+
+if number_day == 1:
+    print('Monday')
+elif number_day == 2:
+    print('Tuesday')
+elif number_day == 3:
+    print('Wednesday')
+elif number_day == 4:
+    print('Thursday')
+elif number_day == 5:
+    print('Friday')
+elif number_day == 6:
+    print('Saturday')
+elif number_day == 7:
+    print('Sunday')
+else:
+    print('Error')
