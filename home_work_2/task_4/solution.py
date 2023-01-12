831852ac76502150fff3deae84ca0fb05e83ea5c
Dmitry Voroshin <dvoroshin13@gmail.com>
2023-01-13 00:45:13 +0500

    Решение 4 задачи

--- home_work_2/task_4/solution.py
+++ home_work_2/task_4/solution.py
@@ -1,10 +1,4 @@
 num = int(input('Введите число: '))
 numbers = str(list(range(-num, num+1)))
 
-for i in range(1, len(numbers)-1):
-    buf = numbers[i]
-    numbers[i] = numbers[:i+3]
-    numbers[:i+3] = buf
-
-for i in range(1, len(numbers)-1):
-    print(numbers[i])
+print(f'{number[-2:]}{numbers[:-2]}}
